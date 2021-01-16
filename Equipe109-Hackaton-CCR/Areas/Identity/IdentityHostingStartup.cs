7b80368d54fd6ebd40f70258ea730da3dab1c105
Cayo Diebe <cayodiebe@gmail.com>
2021-01-16 14:42:56 -0300

    Adicionar Identity

---
+++ Equipe109-Hackaton-CCR/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Equipe109_Hackaton_CCR.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Equipe109_Hackaton_CCR.Areas.Identity.IdentityHostingStartup))]
+namespace Equipe109_Hackaton_CCR.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
