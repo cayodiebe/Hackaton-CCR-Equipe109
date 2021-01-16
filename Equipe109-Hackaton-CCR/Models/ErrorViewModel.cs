d91eca2c5dd57478ca2a63d6b230e22fe266f174
Cayo Diebe <cayodiebe@gmail.com>
2021-01-16 14:16:06 -0300

    Primeiro Commit

---
+++ Equipe109-Hackaton-CCR/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Equipe109_Hackaton_CCR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
