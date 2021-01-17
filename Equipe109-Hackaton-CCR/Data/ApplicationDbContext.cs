a9d179e74c3746bebe574efee200ebd6aff8e452
Cayo Diebe <cayodiebe@gmail.com>
2021-01-17 14:29:59 -0300

    Vagas Candidato - Estrutura Completa

--- Equipe109-Hackaton-CCR/Data/ApplicationDbContext.cs
+++ Equipe109-Hackaton-CCR/Data/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace Equipe109_Hackaton_CCR.Data
         public DbSet<EmpresaModel> EmpresaModels { get; set; }
         public DbSet<VagasModel> VagasModel { get; set; }
         public DbSet<EmpresaLogin> EmpresaLogin { get; set; }
+        public DbSet<VagasCandidatoModel> VagasCandidato { get; set; }
         public DbSet<Home> Home { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
